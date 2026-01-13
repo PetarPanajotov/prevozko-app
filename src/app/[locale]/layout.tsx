8695242efe02e8a66cf5f81b973d02876bc74602
Petar Panajotov <petarpanajotov@gmail.com>
2026-01-13 18:06:06 +0200

    fix: params type with promise

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,8 @@
-import { routing } from "@/i18n/routing";
-import { NextIntlClientProvider } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
-import { notFound } from "next/navigation";
+import { routing } from '@/i18n/routing';
+import { NextIntlClientProvider } from 'next-intl';
+import { setRequestLocale } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { ReactNode } from 'react';
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
@@ -11,20 +12,17 @@ export default async function LocaleLayout({
   children,
   params,
 }: {
-  children: React.ReactNode
-  params: { locale: string }
+  children: ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
 
-  // Ensure that the incoming locale is valid
   if (!routing.locales.includes(locale as 'en' | 'bg')) {
     notFound();
   }
 
-  // Enable static rendering
   setRequestLocale(locale);
 
-  // Load messages
   const messages = (await import(`../../messages/${locale}.json`)).default;
 
   return (
